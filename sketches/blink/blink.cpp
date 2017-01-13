d6e09318761535538dc89a9e14107e0378ff3d16
Amitesh Singh <singh.amitesh@gmail.com>
2017-01-13 22:53:47 +0530

    Add basic project skeleton
    Signed-off-by: Amitesh Singh <singh.amitesh@gmail.com>

---
+++ sketches/blink/blink.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#define LED 2
+
+void setup()
+{
+    pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+    delay(100);
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+}

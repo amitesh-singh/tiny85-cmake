a6e80c732513583c8e84e64c3f4bb82166e691dc
Amitesh Singh <singh.amitesh@gmail.com>
2017-01-15 14:12:05 +0530

    Add hc165n bitbang read code.
    but this does not since we need to write 0 and read at  the same time.
    currently our shiftIn does not support it. :/

---
+++ src/hc165n-soft/hc165n.cpp
@@ -0,0 +1,56 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+//This is a test done on 74HC595N spi
+
+//NOTE:
+//MOSI and MISO for uploading the code via programmer is PB0 and PB1
+// But for USI based SPI communication, Dataout or D0 is PB1 and Datain or DI is PB0. This is opposite to above case. so remember this.
+// connection
+// DO(PB1) - serial data pin 74HC595
+// PB2  --- shift clock
+// PB4  --- latch clock
+// OE - GND  (must)
+// RST - Vcc (must)
+
+#include "spiSoft.h"
+#define LATCH PB4 //or LATCH_PIN or select slave
+int main()
+{
+    spiSoft SPI;
+
+    //uint8_t buttonVal = 0;
+
+    SPI.begin(&DDRB, &PORTB, &PINB, PB0, PB2);
+    DDRB |= (1 << LATCH);
+    DDRB |= (1 << PB3);
+    PORTB &= ~(1 << PB3);
+    PORTB |= (1 << LATCH); //set latch pin HIGH when transferring data
+    PORTB |= (1 << PB3);
+    // The transfer order is LSB Q0 -- Q7
+    _delay_ms(1000);
+    PORTB  &= ~(1 << PB3);
+    _delay_ms(1000);
+    uint8_t data;
+    while (1)
+    {
+        _delay_ms(100);
+        PORTB &= ~(1 << LATCH);
+        PORTB |= (1 << LATCH);
+        //trasfer 1 byte
+        SPI.shiftOut(0);
+        _delay_ms(100);
+        PORTB &= ~(1 << LATCH);
+        PORTB |= (1 << LATCH);
+        SPI.shiftIn(data);
+         
+        if (data >= 1)
+            PORTB &= ~(1 << PB3);
+        else
+            PORTB |= (1 << PB3);
+
+        _delay_ms(10);
+     
+    }
+    return 0;
+}
